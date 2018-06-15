79618cfa0fde6e05df4ad6a05789ef6f26a368d5
E Denison <egdenison@gmail.com>
2018-06-14 22:15:06 -0400

    Implemented solutions for week 2 parts 1-2 for fibonacci numbers.

--- AlgorithmicToolbox/Week2/1_fibonacci_number/fibonacci.py
+++ AlgorithmicToolbox/Week2/1_fibonacci_number/fibonacci.py
@@ -1,9 +1,23 @@
 # Uses python3
-def calc_fib(n):
+# Understand Fibonacci sequence as F[0]==1, F[1]==1, and F[i]==F[i-1]+F[i-2]
+def calc_fib_naive(n):
     if (n <= 1):
         return n
 
     return calc_fib(n - 1) + calc_fib(n - 2)
 
-n = int(input())
-print(calc_fib(n))
+def calc_fib(n):
+    assert(n >= 0)
+
+    fibArray = []
+    for i in range(n):
+        if (i <= 1):
+            fibArray.append(i)
+        else:
+            fibArray.append(fibArray[i-1] + fibArray[i-2])
+    return fibArray[-1]
+
+if __name__=="__main__":
+    n = int(input())
+    #print(calc_fib_naive(n))
+    print(calc_fib(n))
--- AlgorithmicToolbox/Week2/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
+++ AlgorithmicToolbox/Week2/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
@@ -1,4 +1,5 @@
 # Uses python3
+# Calculate the last digit of the nth fibonacci number
 import sys
 
 def get_fibonacci_last_digit_naive(n):
@@ -9,11 +10,29 @@ def get_fibonacci_last_digit_naive(n):
     current  = 1
 
     for _ in range(n - 1):
+        #note, this is tuple destructuring assignment.
+        #equivalent to previous = current and current = previous + current.
+        #"assign previous and current equal to current and (previous + current) respectively"
         previous, current = current, previous + current
 
     return current % 10
 
+def get_fibonacci_last_digit(n):
+    if n <= 1:
+        return n
+
+    previous = 0
+    current  = 1
+
+    for _ in range(n - 1):
+        previous, current = current % 10, (previous + current)%10
+
+    return current % 10
+
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n = int(input)
-    print(get_fibonacci_last_digit_naive(n))
+    n = int(input())
+    # for n1 in range(n):
+    #     if get_fibonacci_last_digit_naive(n1) != get_fibonacci_last_digit(n1):
+    #         print("Wrong answer: n={}, expected:{}, actual:{}".format(n1, get_fibonacci_last_digit_naive(n1), get_fibonacci_last_digit(n1)))
+
+    print(get_fibonacci_last_digit(n))
