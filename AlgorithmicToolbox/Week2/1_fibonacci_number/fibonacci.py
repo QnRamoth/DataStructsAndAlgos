4c3e5c51464d0c6bd99b8d64fbaea6555dda82e0
E Denison <egdenison@gmail.com>
2018-06-17 01:13:49 -0400

    - Repaired bug in original fibonacci solution. - Implemented fibonacci modulus solution.

--- AlgorithmicToolbox/Week2/1_fibonacci_number/fibonacci.py
+++ AlgorithmicToolbox/Week2/1_fibonacci_number/fibonacci.py
@@ -1,23 +1,40 @@
 # Uses python3
-# Understand Fibonacci sequence as F[0]==1, F[1]==1, and F[i]==F[i-1]+F[i-2]
+# Understand Fibonacci sequence as F[0]==0, F[1]==1, and F[i]==F[i-1]+F[i-2]
 def calc_fib_naive(n):
     if (n <= 1):
         return n
 
     return calc_fib(n - 1) + calc_fib(n - 2)
 
-def calc_fib(n):
+def calc_fib_array(n):
     assert(n >= 0)
+    if (n <= 1):
+        return n
 
     fibArray = []
-    for i in range(n):
+    for i in range(n+1):
         if (i <= 1):
             fibArray.append(i)
         else:
             fibArray.append(fibArray[i-1] + fibArray[i-2])
     return fibArray[-1]
 
+def calc_fib(n):
+    if n <= 1:
+        return n
+    previous = 0
+    current = 1
+    for _ in range(n-1):
+        previous, current = current, previous + current
+    return current
+
 if __name__=="__main__":
     n = int(input())
-    #print(calc_fib_naive(n))
+    # print(calc_fib_naive(n))
+    # print(calc_fib_array(n))
     print(calc_fib(n))
+
+    # for n1 in range(2,10000):
+    #     print(calc_fib_naive(n1))
+    #     print(calc_fib_array(n1))
+    #     print(calc_fib(n1))
--- AlgorithmicToolbox/Week2/5_fibonacci_number_again/fibonacci_huge.py
+++ AlgorithmicToolbox/Week2/5_fibonacci_number_again/fibonacci_huge.py
@@ -1,4 +1,6 @@
 # Uses python3
+# These functions report the n-th Fibonacci number modulus m
+# The pisano period is the number of Fibonacci values before a given modulus m begins to repeat.
 import sys
 
 def get_fibonacci_huge_naive(n, m):
@@ -13,7 +15,39 @@ def get_fibonacci_huge_naive(n, m):
 
     return current % m
 
+def get_fibonacci_huge(n, m):
+    if n <= 1:
+        return n
+
+    pisano_period = get_pisano_period(m)
+    equivalent_n = n % pisano_period
+
+    previous = 0
+    current  = 1
+
+    for _ in range(equivalent_n - 1):
+        previous, current = current, previous + current
+
+    return current % m
+
+
+def get_pisano_period(m):
+    previous = 0
+    current = 1
+    fibModulusArray = [previous%m]
+
+    for i in range(m**2+3):
+        previous, current = current, previous + current
+        fibModulusArray.append(previous%m)
+        if(
+            i>3 and 
+            fibModulusArray[-3] == 0 and 
+            fibModulusArray[-2] == 1 and 
+            fibModulusArray[-1] == 1):
+            return i-1
+
 if __name__ == '__main__':
-    input = sys.stdin.read();
-    n, m = map(int, input.split())
-    print(get_fibonacci_huge_naive(n, m))
+    inputs = input()
+    n, m = map(int, inputs.split())
+    # print(get_fibonacci_huge_naive(n, m))
+    print(get_fibonacci_huge(n, m))
