fc6504ff30e1094eca66f2b396b0877a585bdb7a
E Glenn Denison <egdenison@gmail.com>
2018-09-09 17:27:57 -0400

    Implemented majority element and 3-partition quick sort.

--- AlgorithmicToolbox/Week4/2_majority_element/majority_element.py
+++ AlgorithmicToolbox/Week4/2_majority_element/majority_element.py
@@ -1,18 +1,63 @@
 # Uses python3
+import math
 import sys
 
-def get_majority_element(a, left, right):
+
+def get_majority_element_naive(a, left, right):
     if left == right:
         return -1
     if left + 1 == right:
         return a[left]
-    #write your code here
+
+    for val in a:
+        count = 0
+        for scanValue in a:
+            if scanValue == val:
+                count += 1
+        if count > len(a)/2:
+            return val
     return -1
+
+
+def get_majority_element(a, left, right):
+    if left == right:
+        return a[left]
+    elif left + 1 == right:
+        if a[left] == a[right]:
+            return a[left]
+        else:
+            return -1
+    elif left + 2 == right:
+        if (a[left] == a[right]) or (a[left] == a[left+1]):
+            return a[left]
+        elif a[left+1] == a[right]:
+            return a[right]
+        else:
+            return -1
+
+    midpoint = math.floor((left + right)/2)
+    leftMajority = get_majority_element(a, left, midpoint)
+    rightMajority = get_majority_element(a, midpoint+1, right)
+
+    if leftMajority == -1 and rightMajority >= 0:
+        return rightMajority
+    elif rightMajority == -1 and leftMajority >= 0:
+        return leftMajority
+    elif leftMajority == rightMajority:
+        return leftMajority
+    else:
+        return -1
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
-    if get_majority_element(a, 0, n) != -1:
+    if get_majority_element_naive(a, 0, n) != -1:
+        print(1)
+    else:
+        print(0)
+
+    if get_majority_element(a, 0, n-1) != -1:
         print(1)
     else:
         print(0)
--- AlgorithmicToolbox/Week4/3_improving_quicksort/sorting.py
+++ AlgorithmicToolbox/Week4/3_improving_quicksort/sorting.py
@@ -2,35 +2,70 @@
 import sys
 import random
 
-def partition3(a, l, r):
-    #write your code here
-    pass
 
-def partition2(a, l, r):
-    x = a[l]
-    j = l;
-    for i in range(l + 1, r + 1):
+def partition3(a, left, right):
+    m1 = left
+    m2 = right
+
+    x = a[left]
+
+    i = left + 1
+    while i <= m2:
+        if a[i] < x:
+            a[i], a[m1] = a[m1], a[i]
+            m1 += 1
+            i += 1
+        elif a[i] > x:
+            a[i], a[m2] = a[m2], a[i]
+            m2 -= 1
+        elif a[i] == x:
+            i += 1
+
+    return m1, m2
+
+
+def partition2(a, left, right):
+    x = a[left]
+    j = left
+    for i in range(left + 1, right + 1):
         if a[i] <= x:
             j += 1
             a[i], a[j] = a[j], a[i]
-    a[l], a[j] = a[j], a[l]
+    a[left], a[j] = a[j], a[left]
     return j
 
 
-def randomized_quick_sort(a, l, r):
-    if l >= r:
+def randomized_quick_sort(a, left, right):
+    if left >= right:
         return
-    k = random.randint(l, r)
-    a[l], a[k] = a[k], a[l]
-    #use partition3
-    m = partition2(a, l, r)
-    randomized_quick_sort(a, l, m - 1);
-    randomized_quick_sort(a, m + 1, r);
+    k = random.randint(left, right)
+    a[left], a[k] = a[k], a[left]
+
+    m = partition2(a, left, right)
+    randomized_quick_sort(a, left, m - 1)
+    randomized_quick_sort(a, m + 1, right)
+
+
+def randomized_quick_sort3(a, left, right):
+    if left >= right:
+        return
+    k = random.randint(left, right)
+    a[left], a[k] = a[k], a[left]
+
+    m = partition3(a, left, right)
+    randomized_quick_sort3(a, left, m[0] - 1)
+    randomized_quick_sort3(a, m[1] + 1, right)
 
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
-    randomized_quick_sort(a, 0, n - 1)
+    # b = a.copy()
+    # randomized_quick_sort(b, 0, n - 1)
+    # for x in b:
+    #     print(x, end=' ')
+    # print('')
+
+    randomized_quick_sort3(a, 0, n - 1)
     for x in a:
         print(x, end=' ')
